df58aaee8b32eea4c92cb6e053e83f6d94067a6a
leomarq <leoglmarques2@gmail.com>
2022-11-08 22:46:25 -0300

    dado virtual

---
+++ javascript/dado-virtual/dado.js
@@ -0,0 +1,23 @@
+// selecionar todos os elementos
+
+let numeroSorteado = 0;
+let imgDado = document.querySelector('#imgDado');
+let btnSortear = document.querySelector('#btnSortear');
+
+let sorteado = document.querySelector('#sorteado');
+
+btnSortear.addEventListener('click',sortear);
+
+function sortear () {
+    imgDado.classList.add('animar');
+    sorteado.classList.add('aparecer');
+    numeroSorteado = getRandomInt(1,6);
+    imgDado.setAttribute('src','img/' + numeroSorteado + '.png');
+    imgDado.classList.remove('animar');
+}
+
+function getRandomInt (min, max) {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
