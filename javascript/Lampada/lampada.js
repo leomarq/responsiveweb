07e15b9ee708e4bcddfb8b269dc4f28157ec9592
leomarq <leoglmarques2@gmail.com>
2022-11-08 22:18:33 -0300

    lampada final

--- javascript/Lampada/lampada.js
+++ javascript/Lampada/lampada.js
@@ -1,12 +1,15 @@
-//ou .querySelector, ambos são formas de selecionar o elemento pelo ID
-let ligar = document.getElementById('ligar');
+let ligar = document.getElementById('ligar'); //ou .querySelector, ambos são formas de selecionar o elemento pelo ID
 let desligar = document.getElementById('desligar');
 let lampada = document.getElementById('lampada');
 
 ligar.addEventListener('click',ligarLampada);
 desligar.addEventListener('click',desligarLampada);
-
 lampada.addEventListener('dblclick',quebrarLampada);
+
+/* outras formas de fazer:
+lampada.addEventListener('mousehover',ligar/desligar/quebrarLampada)
+lampada.addEventListener('mouseleave',ligar/desligar/quebrarLampada)
+*/
 
 function ligarLampada () {
     lampada.src = 'img/ligada.jpg';
